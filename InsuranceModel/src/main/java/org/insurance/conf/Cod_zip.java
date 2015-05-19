4a67464e0b8b3d780d407ac7a58a37879d6c3cf9
xfrancisco <xavier.francisco@canal-plus.com>
2015-05-19 20:01:11 +0200

    Encore managers

--- InsuranceModel/src/main/java/org/insurance/conf/Cod_zip.java
+++ InsuranceModel/src/main/java/org/insurance/conf/Cod_zip.java
@@ -1,17 +1,31 @@
 package org.insurance.conf;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "COD_ZIP")
-public class Cod_zip {
+public class Cod_zip implements Serializable {
+
+	private static final long serialVersionUID = 2470684085717690736L;
 
 	@Id
 	private String zipcode;
 	@Id
 	private String city;
+	@Id
+	private String ccountry;
+
+	public String getCcountry() {
+		return ccountry;
+	}
+
+	public void setCcountry(String ccountry) {
+		this.ccountry = ccountry;
+	}
 
 	private String indvali;
 
